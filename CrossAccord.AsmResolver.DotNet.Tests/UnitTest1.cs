61b57b00aadd23545fb290759613521a8e2cc49c
Lillie <75392499+iLillie@users.noreply.github.com>
2026-04-02 12:10:12 +0200

    add: csproj CrossAccord.AsmResolver.DotNet.Test

---
+++ CrossAccord.AsmResolver.DotNet.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace CrossAccord.AsmResolver.DotNet.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
