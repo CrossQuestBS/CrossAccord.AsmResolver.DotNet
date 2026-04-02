03d853dd03fd279dfacbc5865432919cbd41a0c8
Lillie <75392499+iLillie@users.noreply.github.com>
2026-04-02 12:58:04 +0200

    add testcases project

---
+++ CrossAccord.AsmResolver.DotNet.TestCases/CIL/CILModificationTestCase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CrossAccord.AsmResolver.DotNet.TestCases.CIL;
+
+public class CILModificationTestCase
+{
+    public void SimpleFunction()
+    {
+        var loadStr = "Hello!";
+        var call = String.Copy(loadStr);
+        Console.WriteLine(call);
+    }
+}
---
+++ CrossAccord.AsmResolver.DotNet.Tests/CIL/CILProcessor_Tests.cs
@@ -0,0 +1,25 @@
+using AsmResolver.DotNet.Code.Cil;
+
+namespace CrossAccord.AsmResolver.DotNet.Tests.CIL;
+
+public class CILProcessor_Tests
+{
+    private CilMethodBody _methodBody;
+    
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [TestFixture]
+    public class InsertAfter : CILProcessor_Tests
+    {
+        
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
